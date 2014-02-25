a2516ea309406781f1a6b20f5ffa0dc35ac156b9
mama <tomas.kulich@gmail.com>
2014-02-25 20:22:39 +0100

    logging added

--- lib/useful.dart
+++ lib/useful.dart
@@ -3,6 +3,7 @@ library useful;
 import 'dart:collection';
 import 'dart:math' as math;
 import 'package:range/range.dart';
+import 'package:logging/logging.dart';
 
 part 'tuple.dart';
 part 'priority_map.dart';
@@ -11,4 +12,5 @@ part 'uitertools.dart';
 part 'variation.dart';
 part 'generator.dart';
 part 'frozen_set.dart';
+part 'logging.dart';
 
