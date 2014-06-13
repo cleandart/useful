676cef63883b604baf79ae3394a438cd22f7ba74
Marcela Hrda <aklecram@gmail.com>
2014-06-13 16:11:21 +0200

    More file functions added.

--- lib/fileutils.dart
+++ lib/fileutils.dart
@@ -1,17 +1,62 @@
 library useful.fileutils;
 
+import 'dart:async';
 import 'dart:io';
 
 /**
  * Writes data to file provided. [file] can be either [File] or [String], in
  * latter case it is expected to be path to the file. If [recursive] is false,
  * the default, the file is created only if all directories in the path exist.
- *  If [recursive] is true, all non-existing path components are created.
+ * If [recursive] is true, all non-existing path components are created.
  */
-writeAsString(dynamic file, String data, {recursive: false}) {
-  File _f;
-  if (file is File) _f = file;
-  else if (file is String) _f = new File(file);
+Future<File> writeAsString(dynamic file, String data, {recursive: false}) {
+  File _f = _toFile(file);
+  return _writeAsSth(_f, _f.writeAsString, data, recursive);
+}
+
+/**
+ * Writes data to file provided. [file] can be either [File] or [String], in
+ * latter case it is expected to be path to the file. If [recursive] is false,
+ * the default, the file is created only if all directories in the path exist.
+ * If [recursive] is true, all non-existing path components are created.
+ */
+Future<File> writeAsBytes(dynamic file, String data, {recursive: false}) {
+  File _f = _toFile(file);
+  return _writeAsSth(_f, _f.writeAsBytes, data, recursive);
+}
+
+/**
+ * Writes data to file provided. [file] can be either [File] or [String], in
+ * latter case it is expected to be path to the file. If [recursive] is false,
+ * the default, the file is created only if all directories in the path exist.
+ * If [recursive] is true, all non-existing path components are created.
+ */
+writeAsStringSync(dynamic file, String data, {recursive: false}) {
+  File _f = _toFile(file);
+  return _writeAsSth(_f, _f.writeAsStringSync, data, recursive);
+}
+
+/**
+ * Writes data to file provided. [file] can be either [File] or [String], in
+ * latter case it is expected to be path to the file. If [recursive] is false,
+ * the default, the file is created only if all directories in the path exist.
+ * If [recursive] is true, all non-existing path components are created.
+ */
+void writeAsBytesSync(dynamic file, String data, {recursive: false}) {
+  File _f = _toFile(file);
+  _writeAsSth(_f, _f.writeAsBytesSync, data, recursive);
+}
+
+_writeAsSthSync(File f, write(data), data, recursive) {
+  f.createSync(recursive: recursive);
+  return write(data);
+}
+
+Future<File> _writeAsSth(File f, write(data), data, recursive) =>
+    f.create(recursive: recursive).then((_) => write(data));
+
+File _toFile(dynamic file) {
+  if (file is File) return file;
+  else if (file is String) return new File(file);
   else throw new ArgumentError('Type of file can only be String or File!');
-  return _f.create(recursive: true).then((_) => _f.writeAsString(data));
 }
