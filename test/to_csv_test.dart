1ea69b193743050074253cc8a677cbd54e293c22
jurom <jurajmchc@gmail.com>
2014-07-01 12:52:37 +0200

    added map to CSV convertor

--- test/to_csv_test.dart
+++ test/to_csv_test.dart
@@ -4,15 +4,60 @@ import 'package:unittest/unittest.dart';
 import 'package:useful/csv.dart';
 
 main() {
-  test('to CSV', () {
-      List<List> data = [['name', 'age', 'color'],
+  run();
+}
+
+run() {
+  group('CSV converts ', () {
+    var listedData;
+    var mappedData;
+    var expectedResult;
+
+    setUp(() {
+      listedData = [['name', 'age', 'color'],
                          ['John', 2, 'blue'],
                          ['Jessica', 20, 'bro"wn'],
                          ['Diana', '\'34\'', null]];
-      String expectedResult = '"name","age","color";'
+      mappedData = [];
+      for (int i = 1; i < listedData.length; i++) {
+        mappedData.add(new Map.fromIterables(listedData[0], listedData[i]));
+      }
+      expectedResult = '"name","age","color";'
                               '"John","2","blue";'
                               '"Jessica","20","bro""wn";'
                               '"Diana","\'34\'","null"';
-      expect(toCSV(data, linesep: ';'), expectedResult);
+    });
+    test('list to CSV', () {
+      expect(listToCSV(listedData, linesep: ';'), expectedResult);
+    });
+    test('map to CSV', () {
+
+      var data = [{
+        "name" : "juro",
+        "age" : "21"
+      },
+      {
+        "name" : "peto",
+        "color" : "blue"
+      },
+      { "name" : "mato",
+        "age" : "50"
+      }];
+
+      var expectedJoined = '"name","age";'
+          '"juro","21";'
+          '"peto","null";'
+          '"mato","50"';
+
+      expect(mapToCSV(data, header: ["name","age"], linesep: ';'), expectedJoined);
+    });
+
+
+
+    test('should use appropriate conversion', () {
+      expect(toCSV(listedData, linesep: ';'), expectedResult);
+      expect(toCSV(mappedData, linesep: ';'), expectedResult);
+    });
+
   });
 }
