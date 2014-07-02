7510e70d2dd0e2a570018d9338fb7492f04b534d
Marcela Hrda <aklecram@gmail.com>
2014-07-02 21:31:14 +0200

    Adding header to all convert-to-csv functions.

--- test/to_csv_test.dart
+++ test/to_csv_test.dart
@@ -8,63 +8,57 @@ main() {
 }
 
 run() {
-  group('CSV converts ', () {
-    var listedData;
-    var mappedData;
-    var expectedResult;
+  group('Convert to CSV format', () {
+    var header = ['name', 'age', 'color'];
+    var data =  [['John', 2, 'blue'],
+                 ['Jessica', 20, 'bro"wn'],
+                 ['Diana', '\'34\'', null]];
 
-    setUp(() {
-      listedData =  [['name', 'age', 'color'],
-                     ['John', 2, 'blue'],
-                     ['Jessica', 20, 'bro"wn'],
-                     ['Diana', '\'34\'', null]];
-      mappedData = [];
-      for (int i = 1; i < listedData.length; i++) {
-        mappedData.add(new Map.fromIterables(listedData[0], listedData[i]));
-      }
-      expectedResult ='"name","age","color";'
-                      '"John","2","blue";'
-                      '"Jessica","20","bro""wn";'
-                      '"Diana","\'34\'","null"';
+    test('for list of lists with header.', () {
+      var expected ='name,age,color;'
+                    '"John","2","blue";'
+                    '"Jessica","20","bro""wn";'
+                    '"Diana","\'34\'","null"';
+      expect(listToCSV(data, header: header, linesep: ';'), expected);
     });
 
-    test('list to CSV', () {
-      expect(listToCSV(listedData, linesep: ';'), expectedResult);
+    test('for list of lists without header.', () {
+      var expected ='"John","2","blue";'
+                    '"Jessica","20","bro""wn";'
+                    '"Diana","\'34\'","null"';
+      expect(listToCSV(data, linesep: ';'), expected);
     });
+  });
 
-    test('map to CSV', () {
-      var data = [{
-        "name" : "juro",
-        "age" : "21"
-      },
-      {
-        "name" : "peto",
-        "color" : "blue"
-      },
-      { "name" : "mato",
-        "age" : "50"
-      }];
+  test('Convert to CSV format for list of maps.', () {
+    var data = [{"name" : "juro", "age" : "21"},
+                {"name" : "peto", "color" : "blue"},
+                {"name" : "mato", "age" : "50"}];
 
-      var expectedJoined ='"name","age";'
-                          '"juro","21";'
-                          '"peto","null";'
-                          '"mato","50"';
-      expect(mapToCSV(data, header: ["name","age"], linesep: ';'), expectedJoined);
-    });
+    var expectedJoined ='name,age;'
+                        '"juro","21";'
+                        '"peto","null";'
+                        '"mato","50"';
+    expect(mapToCSV(data, header: ["name","age"], linesep: ';'), expectedJoined);
+  });
 
-    test('should use appropriate conversion', () {
-      expect(toCSV(listedData, linesep: ';'), expectedResult);
-      expect(toCSV(mappedData, linesep: ';'), expectedResult);
-    });
+  test('Convert to CSV format uses appropriate conversion.', () {
+    var header = ['a', 'b', 'c'];
+    var listedData = [[1, 2, 3], [4, 5, 6]];
+    var mappedData = [{'a': 1, 'b': 2, 'c': 3}, {'a': 4, 'b': 5, 'c': 6}];
+    var expected = 'a,b,c;"1","2","3";"4","5","6"';
 
-    test('get keys from maps', () {
-      var maps = [
-        {"a" : 1, "b" : 1, "e" : 1},
-        {"b" : 1, "c" : 1, "e" : 1},
-        {"e" : 1, "f" : 1, "a" : 1, "c" : 1},
-        {"g" : 1}];
-      expect(getKeysFromMaps(maps), unorderedEquals(["a","b","c","e","f","g"]));
-    });
+    expect(toCSV(listedData, header: header, linesep: ';'), expected);
+    expect(toCSV(mappedData, header: header, linesep: ';'), expected);
+    expect(toCSV(mappedData, linesep: ';'), expected);
+  });
 
+  test('Get keys from maps.', () {
+    var maps = [
+      {"a" : 1, "b" : 1, "e" : 1},
+      {"b" : 1, "c" : 1, "e" : 1},
+      {"e" : 1, "f" : 1, "a" : 1, "c" : 1},
+      {"g" : 1}];
+    expect(getKeysFromMaps(maps), unorderedEquals(["a","b","c","e","f","g"]));
   });
 }
